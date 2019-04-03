131145c67fec8d69decfd74ac7f9b913f44e7244
Алексей Бельченков <u608110@gmail.com>
2019-04-03 16:14:21 +0400

    Output Products in UI

--- js/app.js
+++ js/app.js
@@ -16,8 +16,17 @@ class Products {
         try {
             let result = await fetch('/products.json');
             let data = await result.json();
+            let products = data.items;
 
-            return data;
+            products = products.map(item => {
+               const { title, price } = item.fields;
+               const { id } = item.sys;
+               const image = item.fields.image.fields.file.url;
+
+               return {title, price, id, image};
+            });
+
+            return products;
         } catch (error) {
             console.log(error);
         }
@@ -26,7 +35,26 @@ class Products {
 
 // display products
 class UI {
+    displayProducts(products) {
+        let result = '';
 
+        products.forEach(product => {
+            result += `
+                <article class="product">
+                    <div class="img-container">
+                        <img src=${product.image} alt="product" class="product-img">
+                        <button class="bag-btn" data-id=${product.id}>
+                            <i class="fas fa-shopping-cart"></i> add to bag
+                        </button>
+                    </div>
+                    <h3>${product.title}</h3>
+                    <h4>$${product.price}</h4>
+                </article>
+            `;
+        });
+
+        productsDOM.innerHTML = result;
+    }
 }
 
 // local storage
@@ -40,5 +68,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // get all products
     products.getProducts()
-        .then(data => console.log(data));
+        .then(products => ui.displayProducts(products));
 });
