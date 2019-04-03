2748a29442699e7c75fd9c286fade37f811701a9
Алексей Бельченков <u608110@gmail.com>
2019-04-03 11:32:07 +0400

    Initialize Variables

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartContent = document.querySelector('.cart-content');
+const cartTotal = document.querySelector('.cart-total');
+const productsDOM = document.querySelector('.products-center');
+
+// cart
+let cart = [];
+
+//
+class Products {
+
+}
