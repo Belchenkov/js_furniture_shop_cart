20a3e7eeae40a234f3349a38e61dee06e27df7c5
Алексей Бельченков <u608110@gmail.com>
2019-04-04 17:53:53 +0400

    Show & Close Cart

--- js/app.js
+++ js/app.js
@@ -135,6 +135,23 @@ class UI {
         cartOverlay.classList.add('transparentBcg');
         cartDOM.classList.add('showCart');
     }
+
+    hideCart() {
+        cartOverlay.classList.remove('transparentBcg');
+        cartDOM.classList.remove('showCart');
+    }
+
+    setupApp() {
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+        cartBtn.addEventListener('click', this.showCart);
+        closeCartBtn.addEventListener('click', this.hideCart);
+    }
+
+    populateCart(cart) {
+        cart.forEach(item => this.addCartItem(item));
+    }
 }
 
 // local storage
@@ -152,11 +169,18 @@ class Storage {
     static saveCart() {
         localStorage.setItem('cart', JSON.stringify(cart));
     }
+
+    static getCart() {
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
+
+    // setup app
+    ui.setupApp();
 
     // get all products
     products.getProducts()
