8f927807606d64afb3577c71310a82e129870967
Алексей Бельченков <u608110@gmail.com>
2019-04-04 11:47:57 +0400

    Set Cart in localStorage

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ const productsDOM = document.querySelector('.products-center');
 
 // cart
 let cart = [];
+// buttons
+let buttonsDOM = [];
 
 // Getting the products
 class Products {
@@ -55,12 +57,54 @@ class UI {
 
         productsDOM.innerHTML = result;
     }
+
+    getBagButtons() {
+        const buttons = [...document.querySelectorAll(".bag-btn")];
+        buttonsDOM = buttons;
+
+        buttons.forEach(button => {
+            let id = button.dataset.id;
+            let inCart = cart.find(item => item.id === id);
+
+            if (inCart) {
+                button.innerText = "In Cart";
+                button.disabled = true;
+            } else {
+                button.addEventListener('click', event => {
+                    event.target.innerText = "In Cart";
+                    event.target.disabled = true;
+                    // get product from products
+                    let cartItem = {...Storage.getProduct(id), amount: 1};
+
+                    // add product to the cart
+                    cart = [...cart, cartItem];
+
+                    // save cart in local storage
+                    Storage.saveCart(cart);
+
+                    // set cart values
+                    // display cart item
+                    // show the cart
+                });
+            }
+        });
+    }
 }
 
 // local storage
 class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products));
+    }
+
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+
+        return products.find(product => product.id === id);
+    }
+
+    static saveCart() {
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 }
 
@@ -73,5 +117,8 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(products => {
             ui.displayProducts(products);
             Storage.saveProducts(products);
+        })
+        .then(() => {
+            ui.getBagButtons();
         });
 });
